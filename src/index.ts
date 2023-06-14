72250b33595ae03a1e401e8646cd292cfb14e680
TOUFIQUER RAHMAN <toufiquerabc@gmail.com>
2023-06-15 04:55:53 +0600

    check done

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
@@ -17,6 +17,10 @@ app.use(cors())
 const runningServer = async (): Promise<void> => {
   try {
     await mongoose.connect(process.env.MONGODB_URL as string)
+
+    app.get('/', (req: Request, res: Response) => {
+      res.send({ message: 'Server is running' })
+    })
 
     app.listen(port, () => console.log('Mongo db is connected'))
   } catch (error) {
